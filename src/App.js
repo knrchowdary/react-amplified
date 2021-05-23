f5a1c614a21ea281d6e512c83b012e2ef75a421f
narasimha rao <narasimha.rao@nitorinfotech.com>
2021-05-23 18:01:44 +0530

    changed message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Amplify!!
+          Hello this is feature branch!!
         </p>
         <a
           className="App-link"
