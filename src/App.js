83d19cb3c5fa1bd8b8aadb1061346ced78ceb1c6
knrchowdary <40348872+knrchowdary@users.noreply.github.com>
2021-05-25 14:48:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Amplify!!
+          Hello from Amplify Test!!
         </p>
         <a
           className="App-link"
